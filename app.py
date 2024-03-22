13e4b349f095a3fe61f9fecd76ff1fecc3b55eff
Nihar Kajla <niharkajla28@gmail.com>
2024-03-22 19:32:42 +0530

    Added files for a template web page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-
+from http import HTTPStatus
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
